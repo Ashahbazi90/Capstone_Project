8ab372ce43848663224e0388067a632f285a695a
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2024-06-26 20:41:05 -0700

    added delay in time for each request

--- extract_coinbase.py
+++ extract_coinbase.py
@@ -1,8 +1,32 @@
 import pandas as pd
 import requests
 import datetime
+import time
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta, timezone
+from dateutil.relativedelta import relativedelta
+import pytz
 
-def fetch_historical_data(product_id, start, end, granularity):
+
+def fetch_historical_data():
+    ## set variables for dates, we are starting with 3 months worth of days and going up to
+    ## a week from the current date as the current week will be out test set
+
+    ## Set timezone so the data is consistent
+    pst = pytz.timezone('US/Pacific')
+    # Get today's date
+    today = datetime.now(pst)
+
+    ## set variable parameters for start, end and granulairty
+    product_id = 'BTC-USD' ## turn this to a list to get more than BTC
+    # start date is 3 months ago
+    start = today - relativedelta(months=3)
+    # end date is a week ago
+    end = today - relativedelta(days=7)
+    granularity = 86400 
+
+    ## set URL to perform get request
     url = f'https://api.pro.coinbase.com/products/{product_id}/candles'
     params = {
         'start': start,
@@ -10,8 +34,9 @@ def fetch_historical_data(product_id, start, end, granularity):
         'granularity': granularity
     }
     response = requests.get(url, params=params)
+    time.sleep(30)
     data = response.json()
-    print(data)
+   # print(data)
     
     # Convert the data to a pandas DataFrame for better readability
     df = pd.DataFrame(data, columns=['time', 'low', 'high', 'open', 'close', 'volume'])
@@ -22,14 +47,33 @@ def fetch_historical_data(product_id, start, end, granularity):
      
     return df
 
-# Define the parameters
-product_id = 'BTC-USD'
-start = '2022-01-01T00:00:00Z'
-end = '2023-01-10T00:00:00Z'
-granularity = 86400  # 1 day in seconds
+# # Define the parameters
+# product_id = 'BTC-USD'
+# start = '2022-01-01T00:00:00Z'
+# end = '2022-01-30T00:00:00Z'
+# granularity = 86400  # 1 day in seconds
 
-# Fetch the historical data
-df = fetch_historical_data(product_id, start, end, granularity)
-
-# Display the data
+# # Fetch the historical data
+df = fetch_historical_data()
+create_raw_file = df.to_csv('RAW_Data/BTC_historical.csv')
 print(df)
+
+# # Display the data
+# #print(df)
+
+# # set timezone to PST 
+# pst = pytz.timezone('US/Pacific')
+# # Get today's date
+# today = datetime.now(pst)
+# seven_days_ago = today - relativedelta(days=7)
+
+# # Calculate the date 3 months ago
+# three_months_ago = today - relativedelta(months=3)
+
+# # Get the ISO format of that date
+# iso_format_date = three_months_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
+# iso_format_date_7 = seven_days_ago.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+# print(f"start = '{iso_format_date_7}'")
+# print(today.strftime('%Y-%m-%dT%H:%M:%SZ'))
