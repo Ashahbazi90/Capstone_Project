ec599321bb3e6f6734cabbf39cb0271b35ccf13b
Justin Farnan <77633929+justinm329@users.noreply.github.com>
2024-07-11 10:40:31 -0400

    updated mysql conn
    combined mysql_conn and mysql_objects into mysql_conn

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Capstone_Project
+
+
+### Direction for running code to set up tables and load data
+
+1. Create a env file with the following defined 
+    HOST_NAME="localhost"
+    USERNAME="root"
+    PASSWORD='yourpassword'
+Make sure to enclose everything in quotes or else it won't pick up on it.
+
+2. When you ahve the env file uploaded, open your MySQL workbench and have it open in the background
--- utils/mysql_conn.py
+++ utils/mysql_conn.py
@@ -41,3 +41,40 @@ class db_conn:
     def close(self, connection):
         connection.close()
 
+
+def create_capstone_db():
+    try:
+        db = db_conn()
+        connection = db.mysql_conn()
+        db.execute_query(connection, "CREATE DATABASE IF NOT EXISTS CAPSTONE")
+        print(f'Sucessful Creation on Database')
+    except mysql.connector.Error as e:
+        print(f'An Error has occured {e}')
+
+def create_historical_price_table():
+    try:
+        db = db_conn()
+        connection = db.mysql_conn()
+        db.execute_query(connection, "USE CAPSTONE")
+        query = """
+                CREATE TABLE IF NOT EXISTS HISTORIC_CRYPTO_PRICES (
+                    time DATE,
+                    low FLOAT,
+                    high FLOAT,
+                    open FLOAT,
+                    close FLOAT,
+                    volume FLOAT,
+                    price_change FLOAT,
+                    average_price FLOAT,
+                    volatility FLOAT,
+                    product_id VARCHAR(255),
+                    load_dt TIMESTAMP 
+                    )"""
+        db.execute_query(connection, query)
+        print(f'Sucessful Creation on Table')
+    except mysql.connector.Error as e:
+        print(f'An Error has occured {e}')
+
+if __name__ == "__main__":
+    create_capstone_db()
+    create_historical_price_table()
